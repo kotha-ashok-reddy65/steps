87bf949ca163a9d772e35463c7379f6c3c2ec0fe
kotha-ashok-reddy65 <ashokreddykotha65@gmail.com>
2023-09-17 23:00:14 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+const messages = [
+  "Code and Decode 🧑‍💻",
+  "I Love JavaScript ❤️",
+  "React is awesome 🥳",
+];
 
 function App() {
+  //let step = 1;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Steps />
+    </>
+  );
+}
+function Steps() {
+  const [step, Setstep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  function previousHandler() {
+    if (step > 1) {
+      Setstep((s) => s - 1);
+    }
+  }
+  function hideOpenHandler() {
+    setIsOpen(!isOpen);
+    // isOpen(false);
+  }
+  // setIsOpen(false);
+  function nextHandler() {
+    if (step < 3) {
+      Setstep((s) => s + 1);
+    }
+  }
+  return (
+    <>
+      <button className="close" onClick={hideOpenHandler}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step === 1 ? "active" : ""}>1</div>
+            <div className={step === 2 ? "active" : ""}>2</div>
+            <div className={step === 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step} : {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={previousHandler}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={nextHandler}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
